c13507a286262b135d6be06c209b3b17320942c6
Balthazar Gronon <bgronon@gmail.com>
2016-08-14 18:45:03 +0200

    feat(core): ability to remove and rebind listeners
    Related to #13.

--- src/index.js
+++ src/index.js
@@ -9,12 +9,25 @@ export default class Zeroconf extends EventEmitter {
     super(props)
 
     this._services = {}
+    this._dListeners = {}
 
-    DeviceEventEmitter.addListener('RNZeroconfStart', () => this.emit('start'))
-    DeviceEventEmitter.addListener('RNZeroconfStop', () => this.emit('stop'))
-    DeviceEventEmitter.addListener('RNZeroconfError', err => this.emit('error', err))
+    this.addDeviceListeners()
+  }
 
-    DeviceEventEmitter.addListener('RNZeroconfFound', service => {
+  /**
+   * Add all event listeners
+   */
+  addDeviceListeners () {
+
+    if (Object.keys(this._dListeners).length) {
+      return this.emit('error', new Error('RNZeroconf listeners already in place.'))
+    }
+
+    this._dListeners.start = DeviceEventEmitter.addListener('RNZeroconfStart', () => this.emit('start'))
+    this._dListeners.stop = DeviceEventEmitter.addListener('RNZeroconfStop', () => this.emit('stop'))
+    this._dListeners.error = DeviceEventEmitter.addListener('RNZeroconfError', err => this.emit('error', err))
+
+    this._dListeners.found = DeviceEventEmitter.addListener('RNZeroconfFound', service => {
       if (!service || !service.name) { return }
       const { name } = service
 
@@ -23,7 +36,7 @@ export default class Zeroconf extends EventEmitter {
       this.emit('update')
     })
 
-    DeviceEventEmitter.addListener('RNZeroconfRemove', service => {
+    this._dListeners.remove = DeviceEventEmitter.addListener('RNZeroconfRemove', service => {
       if (!service || !service.name) { return }
       const { name } = service
 
@@ -33,7 +46,7 @@ export default class Zeroconf extends EventEmitter {
       this.emit('update')
     })
 
-    DeviceEventEmitter.addListener('RNZeroconfResolved', service => {
+    this._dListeners.resolved = DeviceEventEmitter.addListener('RNZeroconfResolved', service => {
       if (!service || !service.name) { return }
 
       this._services[service.name] = service
@@ -41,6 +54,14 @@ export default class Zeroconf extends EventEmitter {
       this.emit('update')
     })
 
+  }
+
+  /**
+   * Remove all event listeners and clean map
+   */
+  removeDeviceListeners () {
+    Object.keys(this._dListeners).forEach(name => this._dListeners[name].remove())
+    this._dListeners = {}
   }
 
   /**
