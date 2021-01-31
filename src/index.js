balthazar/react-native-zeroconf

commit aae18fd1f4fc146c4942d23abffc421ab3b2ffab

Adding support for DNSSD (#131)
* Adding support for DNSSD

* Parameterizing the implementation type to use

* Fixing issues with crashes and exception handling

src/index.js
@@ -3,7 +3,15 @@ import { EventEmitter } from 'events'
 
 const RNZeroconf = NativeModules.RNZeroconf
 
+export const ImplType = {
+  NSD: "NSD",
+  DNSSD: "DNSSD"
+};
+
 export default class Zeroconf extends EventEmitter {
+
+
+
   constructor(props) {
     super(props)
 
@@ -111,34 +119,49 @@ export default class Zeroconf extends EventEmitter {
    * Scan for Zeroconf services,
    * Defaults to _http._tcp. on local domain
    */
-  scan(type = 'http', protocol = 'tcp', domain = 'local.') {
+  scan(type = 'http', protocol = 'tcp', domain = 'local.', implType = ImplType.NSD) {
     this._services = {}
     this.emit('update')
-    RNZeroconf.scan(type, protocol, domain)
+    if (Platform.OS === 'android') {
+      RNZeroconf.scan(type, protocol, domain, implType)
+    } else {
+      RNZeroconf.scan(type, protocol, domain)
+    }
   }
 
   /**
    * Stop current scan if any
    */
-  stop() {
-    RNZeroconf.stop()
+  stop(implType = ImplType.NSD) {
+    if (Platform.OS === 'android') {
+      RNZeroconf.stop(implType)
+    } else {
+      RNZeroconf.stop()
+    }
   }
 
   /**
    * Publish a service
    */
-  publishService(type, protocol, domain = 'local.', name, port, txt = {}) {
+  publishService(type, protocol, domain = 'local.', name, port, txt = {}, implType = ImplType.NSD) {
     if (Object.keys(txt).length !== 0) {
       Object.entries(txt).map(([key, value]) => txt[key] = value.toString());
     }
-
-    RNZeroconf.registerService(type, protocol, domain, name, port, txt);
+    if (Platform.OS === 'android') {
+      RNZeroconf.registerService(type, protocol, domain, name, port, txt, implType);
+    } else {
+      RNZeroconf.registerService(type, protocol, domain, name, port, txt);
+    }
   }
 
   /**
    * Unpublish a service
    */
-  unpublishService(name) {
-    RNZeroconf.unregisterService(name)
+  unpublishService(name, implType = ImplType.NSD) {
+    if (Platform.OS === 'android') {
+      RNZeroconf.unregisterService(name, implType);
+    } else {
+      RNZeroconf.unregisterService(name);
+    }
   }
 }

.watchmanconfig
@@ -1,0 +1,6 @@
+{
+  "ignore_dirs": [
+    ".git",
+    "node_modules"
+  ]
+}

android/build.gradle
@@ -28,6 +28,10 @@ android {
             abiFilters "armeabi-v7a", "x86"
         }
     }
+    compileOptions {
+        sourceCompatibility = 1.8
+        targetCompatibility = 1.8
+    }
 }
 
 repositories {
@@ -37,4 +41,10 @@ repositories {
 
 dependencies {
     implementation "com.facebook.react:react-native:${safeExtGet('reactNativeVersion', '+')}"
+    // https://mvnrepository.com/artifact/com.github.andriydruk/rx2dnssd
+    api group: 'com.github.andriydruk', name: 'rx2dnssd', version: '0.9.13'
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+    api group: 'org.apache.commons', name: 'commons-lang3', version: '3.11'
+
+
 }

android/src/main/java/com/balthazargronon/RCTZeroconf/ZeroConfImplFactory.java
@@ -1,0 +1,51 @@
+package com.balthazargronon.RCTZeroconf;
+
+import com.balthazargronon.RCTZeroconf.nsd.NsdServiceImpl;
+import com.balthazargronon.RCTZeroconf.rx2dnssd.DnssdImpl;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ZeroConfImplFactory {
+
+    public static final String NSD_IMPL = "NSD";
+
+    public static final String DNSSD_IMPL = "DNSSD";
+
+    private Map<String, Zeroconf> zeroconfMap = new HashMap<>();
+
+    private ZeroconfModule zeroconfModule;
+    private ReactApplicationContext context;
+
+    public ZeroConfImplFactory(ZeroconfModule zeroconfModule, ReactApplicationContext context) {
+        this.zeroconfModule = zeroconfModule;
+        this.context = context;
+    }
+
+    public Zeroconf getZeroconf(String implType) {
+        if (StringUtils.isBlank(implType)) implType = ZeroConfImplFactory.NSD_IMPL;
+        return getOrCreateImpl(implType);
+    }
+
+    private Zeroconf getOrCreateImpl(String implType) {
+        if (!zeroconfMap.containsKey(implType)) {
+            switch (implType) {
+                case NSD_IMPL:
+                    zeroconfMap.put(NSD_IMPL, new NsdServiceImpl(zeroconfModule, context));
+                    break;
+                case DNSSD_IMPL:
+                    zeroconfMap.put(DNSSD_IMPL, new DnssdImpl(zeroconfModule, context));
+                    break;
+                default:
+                    throw new IllegalArgumentException(String.format("%s implType is not supported. Only %s and %s are supported", implType, NSD_IMPL, DNSSD_IMPL));
+            }
+        }
+
+        return zeroconfMap.get(implType);
+    }
+
+}

android/src/main/java/com/balthazargronon/RCTZeroconf/Zeroconf.java
@@ -1,0 +1,14 @@
+package com.balthazargronon.RCTZeroconf;
+
+import com.facebook.react.bridge.ReadableMap;
+
+public interface Zeroconf {
+
+    void scan(String type, String protocol, String domain);
+
+    void stop();
+
+    public void unregisterService(String serviceName);
+
+    public void registerService(String type, String protocol, String domain, String name, int port, ReadableMap txt);
+}

android/src/main/java/com/balthazargronon/RCTZeroconf/ZeroconfModule.java
@@ -1,31 +1,17 @@
 package com.balthazargronon.RCTZeroconf;
 
-import android.content.Context;
-import android.net.nsd.NsdManager;
-import android.net.nsd.NsdServiceInfo;
-import android.net.wifi.WifiManager;
-import android.os.Build;
 
-import com.facebook.react.bridge.NativeMap;
+import android.util.Log;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.WritableNativeArray;
-import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import javax.annotation.Nullable;
 
-import java.net.InetAddress;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.io.UnsupportedEncodingException;
 
 /**
  * Created by Jeremy White on 8/1/2016.
@@ -50,24 +36,11 @@ public class ZeroconfModule extends ReactContextBaseJavaModule {
     public static final String KEY_SERVICE_ADDRESSES = "addresses";
     public static final String KEY_SERVICE_TXT = "txt";
 
-    protected NsdManager mNsdManager;
-    protected NsdManager.DiscoveryListener mDiscoveryListener;
-    protected WifiManager.MulticastLock multicastLock;
-
-    protected Map<String, NsdManager.RegistrationListener> mPublishedServices;
+    private ZeroConfImplFactory zeroConfFactory;
 
     public ZeroconfModule(ReactApplicationContext reactContext) {
-
         super(reactContext);
-        mPublishedServices = new HashMap<String, NsdManager.RegistrationListener>();
-    }
-
-    protected
-    NsdManager getNsdManager() {
-        if (mNsdManager == null) {
-            mNsdManager = (NsdManager) getReactApplicationContext().getSystemService(Context.NSD_SERVICE);
-        }
-        return mNsdManager;
+        zeroConfFactory = new ZeroConfImplFactory(this, getReactApplicationContext());
     }
 
     @Override
@@ -76,213 +49,66 @@ public class ZeroconfModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void scan(String type, String protocol, String domain) {
-        if (mNsdManager == null) {
-            mNsdManager = (NsdManager) getReactApplicationContext().getSystemService(Context.NSD_SERVICE);
+    public void scan(String type, String protocol, String domain, String implType) {
+        try {
+            getZeroconfImpl(implType).scan(type, protocol, domain);
+        } catch (Exception e) {
+            Log.e(getClass().getName(), e.getMessage(), e);
+            sendEvent(getReactApplicationContext(), ZeroconfModule.EVENT_ERROR, "Exception During Scan: " + e.getMessage());
         }
-
-        this.stop();
-
-        if (multicastLock == null) {
-            WifiManager wifi = (WifiManager) getReactApplicationContext().getSystemService(Context.WIFI_SERVICE);
-            multicastLock = wifi.createMulticastLock("multicastLock");
-            multicastLock.setReferenceCounted(true);
-            multicastLock.acquire();
-        }
-
-        mDiscoveryListener = new NsdManager.DiscoveryListener() {
-            @Override
-            public void onStartDiscoveryFailed(String serviceType, int errorCode) {
-                String error = "Starting service discovery failed with code: " + errorCode;
-                sendEvent(getReactApplicationContext(), EVENT_ERROR, error);
-            }
-
-            @Override
-            public void onStopDiscoveryFailed(String serviceType, int errorCode) {
-                String error = "Stopping service discovery failed with code: " + errorCode;
-                sendEvent(getReactApplicationContext(), EVENT_ERROR, error);
-            }
-
-            @Override
-            public void onDiscoveryStarted(String serviceType) {
-                sendEvent(getReactApplicationContext(), EVENT_START, null);
-            }
-
-            @Override
-            public void onDiscoveryStopped(String serviceType) {
-                sendEvent(getReactApplicationContext(), EVENT_STOP, null);
-            }
-
-            @Override
-            public void onServiceFound(NsdServiceInfo serviceInfo) {
-                WritableMap service = new WritableNativeMap();
-                service.putString(KEY_SERVICE_NAME, serviceInfo.getServiceName());
-
-                sendEvent(getReactApplicationContext(), EVENT_FOUND, service);
-                mNsdManager.resolveService(serviceInfo, new ZeroResolveListener());
-            }
-
-            @Override
-            public void onServiceLost(NsdServiceInfo serviceInfo) {
-                WritableMap service = new WritableNativeMap();
-                service.putString(KEY_SERVICE_NAME, serviceInfo.getServiceName());
-                sendEvent(getReactApplicationContext(), EVENT_REMOVE, service);
-            }
-        };
-
-        String serviceType = String.format("_%s._%s.", type, protocol);
-        mNsdManager.discoverServices(serviceType, NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
     }
 
     @ReactMethod
-    public void stop() {
-        if (mDiscoveryListener != null) {
-            mNsdManager.stopServiceDiscovery(mDiscoveryListener);
+    public void stop(String implType) {
+        try {
+            getZeroconfImpl(implType).stop();
+        } catch (Exception e) {
+            Log.e(getClass().getName(), e.getMessage(), e);
+            sendEvent(getReactApplicationContext(), ZeroconfModule.EVENT_ERROR, "Exception During Stop: " + e.getMessage());
         }
-        if (multicastLock != null) {
-            multicastLock.release();
-        }
-        mDiscoveryListener = null;
-        multicastLock = null;
+    }
+
+    private Zeroconf getZeroconfImpl(String implType) {
+        return zeroConfFactory.getZeroconf(implType);
     }
 
     @ReactMethod
-    public void registerService(String type, String protocol, String domain, String name, int port, ReadableMap txt) {
-        String serviceType = String.format("_%s._%s.", type, protocol);
-
-        final NsdManager nsdManager = this.getNsdManager();
-        NsdServiceInfo serviceInfo  = new NsdServiceInfo();
-        serviceInfo.setServiceName(name);
-        serviceInfo.setServiceType(serviceType);
-        serviceInfo.setPort(port);
-
-        ReadableMapKeySetIterator iterator = txt.keySetIterator();
-        while (iterator.hasNextKey()) {
-            String key = iterator.nextKey();
-            serviceInfo.setAttribute(key, txt.getString(key));
+    public void registerService(String type, String protocol, String domain, String name, int port, ReadableMap txt, String implType) {
+        try {
+            getZeroconfImpl(implType).registerService(type, protocol, domain, name, port, txt);
+        } catch (Exception e) {
+            Log.e(getClass().getName(), e.getMessage(), e);
+            sendEvent(getReactApplicationContext(), ZeroconfModule.EVENT_ERROR, "Exception During Register Service: " + e.getMessage());
         }
-
-        nsdManager.registerService(
-                serviceInfo, NsdManager.PROTOCOL_DNS_SD, new ServiceRegistrationListener());
     }
 
     @ReactMethod
-    public void unregisterService(String serviceName) {
-
-        final NsdManager nsdManager = this.getNsdManager();
-
-        NsdManager.RegistrationListener serviceListener = mPublishedServices.get(serviceName);
-
-        if (serviceListener != null) {
-            mPublishedServices.remove(serviceName);
-            nsdManager.unregisterService(serviceListener);
+    public void unregisterService(String serviceName, String implType) {
+        try {
+            getZeroconfImpl(implType).unregisterService(serviceName);
+        } catch (Exception e) {
+            Log.e(getClass().getName(), e.getMessage(), e);
+            sendEvent(getReactApplicationContext(), ZeroconfModule.EVENT_ERROR, "Exception During Unregister Service: " + e.getMessage());
         }
     }
 
-
-    protected void sendEvent(ReactContext reactContext,
-                             String eventName,
-                             @Nullable Object params) {
+    public void sendEvent(ReactContext reactContext,
+                          String eventName,
+                          @Nullable Object params) {
         reactContext
                 .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                 .emit(eventName, params);
     }
 
-    protected
-    WritableMap serviceInfoToMap(NsdServiceInfo serviceInfo) {
-        WritableMap service = new WritableNativeMap();
-        service.putString(KEY_SERVICE_NAME, serviceInfo.getServiceName());
-        final InetAddress host = serviceInfo.getHost();
-        final String fullServiceName;
-        if (host == null) {
-            fullServiceName = serviceInfo.getServiceName();
-        } else {
-            fullServiceName = host.getHostName() + serviceInfo.getServiceType();
-            service.putString(KEY_SERVICE_HOST, host.getHostName());
-
-            WritableArray addresses = new WritableNativeArray();
-            addresses.pushString(host.getHostAddress());
-
-            service.putArray(KEY_SERVICE_ADDRESSES, addresses);
-        }
-        service.putString(KEY_SERVICE_FULL_NAME, fullServiceName);
-        service.putInt(KEY_SERVICE_PORT, serviceInfo.getPort());
-
-        WritableMap txtRecords = new WritableNativeMap();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Map<String, byte[]> attributes = serviceInfo.getAttributes();
-            for (String key : attributes.keySet()) {
-                try {
-                    byte[] recordValue = attributes.get(key);
-                    txtRecords.putString(String.format(Locale.getDefault(), "%s", key), String.format(Locale.getDefault(), "%s", recordValue != null ? new String(recordValue, "UTF_8") : ""));
-                } catch (UnsupportedEncodingException e) {
-                    String error = "Failed to encode txtRecord: " + e;
-                    sendEvent(getReactApplicationContext(), EVENT_ERROR, error);
-                }
-            }
-        }
-
-        service.putMap(KEY_SERVICE_TXT, txtRecords);
-
-        return service;
-    }
-
-    private class ZeroResolveListener implements NsdManager.ResolveListener {
-        @Override
-        public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
-            if (errorCode == NsdManager.FAILURE_ALREADY_ACTIVE) {
-                mNsdManager.resolveService(serviceInfo, this);
-            } else {
-                String error = "Resolving service failed with code: " + errorCode;
-                sendEvent(getReactApplicationContext(), EVENT_ERROR, error);
-            }
-        }
-
-        @Override
-        public void onServiceResolved(NsdServiceInfo serviceInfo) {
-            WritableMap service = serviceInfoToMap(serviceInfo);
-            sendEvent(getReactApplicationContext(), EVENT_RESOLVE, service);
-        }
-    }
-
-    private class ServiceRegistrationListener implements NsdManager.RegistrationListener {
-
-        @Override
-        public void onServiceRegistered(NsdServiceInfo NsdServiceInfo) {
-            // Save the service name.  Android may have changed it in order to
-            // resolve a conflict, so update the name you initially requested
-            // with the name Android actually used.
-
-            final String serviceName = NsdServiceInfo.getServiceName();
-            mPublishedServices.put(serviceName, this);
-
-            WritableMap service = serviceInfoToMap(NsdServiceInfo);
-            sendEvent(getReactApplicationContext(), EVENT_PUBLISHED, service);
-        }
-
-        @Override
-        public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
-            // Registration failed!  Put debugging code here to determine why.
-        }
-
-        @Override
-        public void onServiceUnregistered(NsdServiceInfo nsdServiceInfo) {
-            // Service has been unregistered.  This only happens when you call
-            // NsdManager.unregisterService() and pass in this listener.
-            final WritableMap service = serviceInfoToMap(nsdServiceInfo);
-            sendEvent(getReactApplicationContext(), EVENT_UNREGISTERED, service);
-        }
-
-        @Override
-        public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
-            // Unregistration failed.  Put debugging code here to determine why.
-        }
-    }
-
     @Override
     public void onCatalystInstanceDestroy() {
         super.onCatalystInstanceDestroy();
-        stop();
+        try {
+            stop(ZeroConfImplFactory.NSD_IMPL);
+            stop(ZeroConfImplFactory.DNSSD_IMPL);
+        } catch (Exception e) {
+            Log.e(getClass().getName(), e.getMessage(), e);
+            sendEvent(getReactApplicationContext(), ZeroconfModule.EVENT_ERROR, "Exception During Catalyst Destroy: " + e.getMessage());
+        }
     }
 }

commit 8e27d2220682839704e5d42ebe8ee7506cb6007a

Fix bug introduced by #131

src/index.js
@@ -1,4 +1,4 @@
-import { NativeModules, DeviceEventEmitter } from 'react-native'
+import { Platform, NativeModules, DeviceEventEmitter } from 'react-native'
 import { EventEmitter } from 'events'
 
 const RNZeroconf = NativeModules.RNZeroconf