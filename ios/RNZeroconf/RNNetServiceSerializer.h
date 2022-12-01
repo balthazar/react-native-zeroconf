e456b3289e901295ade07bd6da9f8602cc4384b9
jerryseigle <mr.jerryseigle@gmail.com>
2022-12-01 04:41:16 -0500

    added address dictionary

--- ios/RNZeroconf/RNNetServiceSerializer.h
+++ ios/RNZeroconf/RNNetServiceSerializer.h
@@ -11,6 +11,7 @@
 FOUNDATION_EXPORT const NSString *kRNServiceKeysName;
 FOUNDATION_EXPORT const NSString *kRNServiceKeysFullName;
 FOUNDATION_EXPORT const NSString *kRNServiceKeysAddresses;
+FOUNDATION_EXPORT const NSString *kRNServiceKeysAddressesDict;
 FOUNDATION_EXPORT const NSString *kRNServiceKeysHost;
 FOUNDATION_EXPORT const NSString *kRNServiceKeysPort;
 FOUNDATION_EXPORT const NSString *kRNServiceTxtRecords;
